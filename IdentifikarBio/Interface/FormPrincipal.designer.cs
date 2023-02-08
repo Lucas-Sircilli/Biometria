d8cf423bd2a081342e8d58c2c2d4304774d2cc83
Lucas-Sircilli <sircilli@hotmail.com>
2023-02-08 17:07:10 -0300

    Uploading the Whole Project

---
+++ IdentifikarBio/Interface/FormPrincipal.designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Biometria
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPrincipal));
+            this.SuspendLayout();
+            // 
+            // FormPrincipal
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormPrincipal";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MenuInicial;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemCadastro;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemRelatorios;
+        private System.Windows.Forms.ToolStripMenuItem MenuItemParametros;
+        private System.Windows.Forms.Label lbData;
+        private System.Windows.Forms.StatusStrip ststBarraInferior;
+        private System.Windows.Forms.ToolStripStatusLabel tstlHora;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ToolStripStatusLabel tstlData;
+        private System.Windows.Forms.ToolStripStatusLabel tstlUsuario;
+        private System.Windows.Forms.ToolStripStatusLabel tstlVersao;
+        private System.Windows.Forms.PictureBox pbLogo;
+    }
+}
+
